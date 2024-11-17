e2dae7818ed4cde8d58432ac5585bccb6e922aa4
vaikunthsuresh <vaikunthsuresh27@gmail.com>
2024-11-17 16:25:52 +0530

    Initial commit

---
+++ src/app/component/site-header/site-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shop-site-header',
+  templateUrl: './site-header.component.html',
+  styleUrl: './site-header.component.css'
+})
+export class SiteHeaderComponent {
+
+}
